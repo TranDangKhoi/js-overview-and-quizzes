903f63e60136f5f4f70b9420a2b6284b73d8deb5
TranDangKhoi <baits16@gmail.com>
2023-06-30 23:57:00 +0700

    :+1: Update

---
+++ 06-typescript-intermediate/app.ts
@@ -0,0 +1,17 @@
+// const calculateSum = (a: number, b: number): number => {
+//   return a + b;
+// };
+
+// calculateSum(12, 34);
+
+// const student: {
+//   name: string;
+//   age: number;
+//   school: string;
+//   phoneNumber: number;
+// } = {
+//   name: "Khoi",
+//   age: 20,
+//   school: "FPTU",
+//   phoneNumber: 89787898,
+// };
